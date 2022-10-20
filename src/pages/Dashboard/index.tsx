e0d4151e44bb0e51f03244e1c1873d742045059c
Vali M <marginean.vali@gmail.com>
2022-10-20 18:11:13 +0300

    #32d98yn: Add Graph Teaser to Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import Actions from './Actions';
-import TopInfo from './TopInfo';
-import Transactions from './Transactions';
 import ReactGA from 'react-ga4';
+import DashboardGraph from './dashboard-graph';
 
 const Dashboard = () => {
   const isLastUrlSegmentEqualTo = (urlSegment: string) => {
@@ -36,19 +34,8 @@ const Dashboard = () => {
   return (
     <div className='container py-4'>
       <div className='row'>
-        <div className='col-12 col-md-10 mx-auto'>
-          <div className='card shadow-sm rounded border-0'>
-            <div className='card-body p-1'>
-              <div className='card rounded border-0 bg-primary'>
-                <div className='card-body text-center p-4'>
-                  <TopInfo />
-                  <Actions />
-                </div>
-              </div>
-              <Transactions />
-            </div>
-          </div>
-        </div>
+        <DashboardGraph />
+        <div className='col-12 col-md-10 mx-auto'></div>
       </div>
     </div>
   );
