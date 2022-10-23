896c1d193cb4c5975342e292f5ed90be4fd63fee
Vali M <marginean.vali@gmail.com>
2022-10-23 22:59:06 +0300

    #32d98yn: Added multiple lines with mock data to graph

--- src/pages/Dashboard/dashboard-graph.tsx
+++ src/pages/Dashboard/dashboard-graph.tsx
@@ -1,7 +1,7 @@
 import { ApexOptions } from 'apexcharts';
 import ReactApexChart from 'react-apexcharts';
 import * as React from 'react';
-import { MockDataDashboardGraph } from './mock-data-dashboard-graph';
+import dashboardGraph from '../../db/dashboardGraph.json';
 
 const DashboardGraph = () => {
   const optionsArea: ApexOptions = {
@@ -9,46 +9,47 @@ const DashboardGraph = () => {
       id: 'area-datetime',
       type: 'area',
       zoom: {
-        autoScaleYaxis: true
-      }
+        // autoScaleYaxis: true
+      },
+      stacked: true,
     },
-    annotations: {
-      yaxis: [
-        {
-          borderColor: '#999',
-          label: {
-            style: {
-              color: '#fff',
-              background: '#00E396'
-            }
-          }
-        }
-      ],
-      xaxis: [
-        {
-          x: new Date().getTime(),
-          borderColor: '#999',
-          // yAxisIndex: 0,
-          label: {
-            style: {
-              color: '#fff',
-              background: '#775DD0'
-            }
-          }
-        }
-      ]
-    },
-    // dataLabels: {
-    //   enabled: false
+    // annotations: {
+    //   yaxis: [
+    //     {
+    //       borderColor: '#999',
+    //       label: {
+    //         style: {
+    //           color: '#fff',
+    //           background: '#00E396'
+    //         }
+    //       }
+    //     }
+    //   ],
+    //   xaxis: [
+    //     {
+    //       x: new Date().getTime(),
+    //       borderColor: '#999',
+    //       // yAxisIndex: 0,
+    //       // label: {
+    //       //   style: {
+    //       //     color: '#fff',
+    //       //     background: '#775DD0'
+    //       //   }
+    //       // }
+    //     }
+    //   ]
     // },
-    markers: {
-      size: 5,
-      shape: 'circle'
+    dataLabels: {
+      enabled: false // do not display number on each point of graph
     },
+    // markers: {
+    //   size: 3,
+    //   shape: 'circle'
+    // },
     xaxis: {
       type: 'datetime',
-      min: new Date('01 Mar 2012').getTime(),
-      tickAmount: 6
+      categories: dashboardGraph.map((el) => el.date)
+      // tickAmount: 6
     },
     tooltip: {
       x: {
@@ -66,13 +67,28 @@ const DashboardGraph = () => {
     }
   };
 
-  const series: ApexAxisChartSeries = MockDataDashboardGraph;
+  const series: ApexOptions['series'] = [
+    {
+      name: 'availableBalance',
+      data: dashboardGraph.map((el) => el.availableBalance)
+    },
+    {
+      name: 'committedBalance',
+      data: dashboardGraph.map((el) => el.committedBalance)
+    },
+    {
+      name: 'total',
+      data: dashboardGraph.map((el) => el.total)
+    }
+  ];
 
   return (
     <ReactApexChart
       options={optionsArea}
       series={series}
       type='area'
+      height='421px'
+      width='1000px'
     ></ReactApexChart>
   );
 };
