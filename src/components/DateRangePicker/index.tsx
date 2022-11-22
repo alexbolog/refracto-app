47f2b8da8182ed3b02c8c3acece973e2ac44a43f
Vali M <marginean.vali@gmail.com>
2022-11-22 13:21:39 +0200

    #3723jhw: custom input date range display

--- src/components/DateRangePicker/index.tsx
+++ src/components/DateRangePicker/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { DateTime } from 'luxon';
 import ReactDatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
 const DateRangePicker = ({
-  startDate,
-  endDate,
+  defaultStartDate = DateTime.now().minus({ years: 1 }),
+  defaultEndDate = DateTime.now(),
   onChange
 }: {
+  defaultStartDate?: DateTime;
+  defaultEndDate?: DateTime;
   startDate?: DateTime;
   endDate?: DateTime;
   onChange?: (startDate: DateTime, endDate: DateTime) => void;
@@ -16,7 +18,28 @@ const DateRangePicker = ({
   const [firstDate, setFirstDate] = React.useState<Date | null>(null);
   const [secondDate, setSecondDate] = React.useState<Date | null>(null);
 
-  function onDatePick(dates: [Date | null, Date | null]) {
+  const renderHeader = (params: any): ReactNode => {
+    debugger;
+    return <></>;
+  };
+
+  const renderInput = (): ReactNode => {
+    const intervalStart = firstDate
+      ? DateTime.fromJSDate(firstDate)
+      : defaultStartDate;
+    const intervalEnd = secondDate
+      ? DateTime.fromJSDate(secondDate)
+      : defaultEndDate;
+    return (
+      <p>
+        {intervalStart.toLocaleString(DateTime.DATE_MED) +
+          '->' +
+          intervalEnd.toLocaleString(DateTime.DATE_MED)}
+      </p>
+    );
+  };
+
+  const onDatePick = (dates: [Date | null, Date | null]) => {
     const startDate = dates[0];
     const endDate = dates[1];
     if (startDate) {
@@ -31,16 +54,18 @@ const DateRangePicker = ({
         endDate ? DateTime.fromJSDate(endDate) : DateTime.now()
       );
     }
-  }
+  };
 
   return (
     <ReactDatePicker
       // selected={firstDate}
+      // renderCustomHeader={renderHeader}
+      customInput={renderInput()}
+      dateFormat={'dd MMM yyyy'}
       onChange={onDatePick}
       startDate={firstDate}
       endDate={secondDate}
       selectsRange={true}
-      // inline
     />
   );
 };
