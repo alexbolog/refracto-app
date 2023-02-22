edf06cb4929b1961dad5f5087bb2afae1f4f690f
alexbolog <mbolog.alex@gmail.com>
2023-02-22 11:01:15 +0200

    added filtering function

--- src/pages/AvailableProjects/index.tsx
+++ src/pages/AvailableProjects/index.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import { ProjectContext } from 'contexts/ProjectContext';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Filters } from './Filters';
 import { ProjectListFilterType } from './Filters/ProjectListFilterType';
 import { Project } from './Project';
 
 const AvailableProjects = () => {
   const { availableProjects } = useContext(ProjectContext);
+  const [currentAppliedFilters, setCurrentAppliedFilters] =
+    useState<ProjectListFilterType>();
   const handleApplyFilters = (filters: ProjectListFilterType) => {
-    //
+    setCurrentAppliedFilters(filters);
   };
+
+  const shouldDisplayProject = () => {
+    return true;
+  };
+
   return (
     <div className='container-fluid w-100 p-0'>
       <div className='row'>
@@ -18,7 +25,7 @@ const AvailableProjects = () => {
         </div>
 
         {availableProjects.length > 0 &&
-          availableProjects.map((p, i) => (
+          availableProjects.filter(shouldDisplayProject).map((p, i) => (
             <div className='col-12' key={`available-project-list-item-${i}`}>
               <Project project={p} />
             </div>
