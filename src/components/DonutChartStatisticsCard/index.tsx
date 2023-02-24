85d3e79bbadafad9c0c4c8ecc9e7a0f40fa80572
Vali M <marginean.vali@gmail.com>
2023-02-24 17:57:23 +0200

    Refactored project investments to hook

--- src/components/DonutChartStatisticsCard/index.tsx
+++ src/components/DonutChartStatisticsCard/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ArcElement, Chart } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
-import myProjects from '../../dbNew/myProjects';
 
 import './style.scss';
 import ExpandFooter from '../ExpandFooter';
 import { toLocaleStringOptions } from '../../config';
+import { GeneralContext } from '../../contexts/GeneralContext';
 
 const DonutChartStatisticsCard = () => {
+  const { activeProjectInvestments } = useContext(GeneralContext);
   Chart.register(ArcElement);
   const hexToRgb = (hex: string) => {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -25,19 +26,17 @@ const DonutChartStatisticsCard = () => {
     return `rgb(${rgb?.r}, ${rgb?.g}, ${rgb?.b})` ?? 'rgb(0,0,0)';
   };
 
-  const colors = ['#6853E8', '#FFA600', '#FF6B45'];
-
-  const totalInvested = myProjects
-    .map((project) => project.crowdfundedAmount)
+  const totalInvested = activeProjectInvestments
+    ?.map((project) => project.amountInvested)
     .reduce((partialSum, crtAmount) => partialSum + crtAmount, 0);
 
   const chartData = {
-    labels: myProjects.map((pl) => pl.projectTitle),
+    labels: activeProjectInvestments?.map((pl) => pl.projectTitle),
     datasets: [
       {
-        data: myProjects.map((pl) => pl.crowdfundedAmount),
-        backgroundColor: myProjects.map((pl, index) =>
-          hexToRgbString(colors[index % 3])
+        data: activeProjectInvestments?.map((pl) => pl.amountInvested),
+        backgroundColor: activeProjectInvestments!.map((pl) =>
+          hexToRgbString(pl.colorCodeHex)
         )
       }
     ]
@@ -71,7 +70,7 @@ const DonutChartStatisticsCard = () => {
           <div className='donut-hole-text'>
             <label>Total Investment</label>
             <label className='fat-number'>
-              €{totalInvested.toLocaleString(undefined, toLocaleStringOptions)}
+              €{totalInvested?.toLocaleString(undefined, toLocaleStringOptions)}
             </label>
           </div>
         </div>
@@ -79,16 +78,16 @@ const DonutChartStatisticsCard = () => {
           <strong>Projects</strong>
         </h4>
         <div className='projects-list container ml-3 mt-2'>
-          {chartData.labels.map((label, idx) => {
+          {chartData.labels?.map((label, idx) => {
             const color = chartData.datasets[0].backgroundColor[idx];
             const roi =
-              myProjects[idx].returnPercentage.toLocaleString(
+              activeProjectInvestments![idx].returnPercentage.toLocaleString(
                 undefined,
                 toLocaleStringOptions
               ) + '%';
             const invested =
               '€' +
-              myProjects[idx].crowdfundedAmount.toLocaleString(
+              activeProjectInvestments![idx].amountInvested.toLocaleString(
                 undefined,
                 toLocaleStringOptions
               );
