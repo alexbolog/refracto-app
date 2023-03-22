28f60036f00b37a976c1f7feae9de9a033f856da
alexbolog <mbolog.alex@gmail.com>
2023-03-22 14:15:52 +0200

    added available cash balance using accountContext

--- src/pages/Invest/InvestmentCard/index.tsx
+++ src/pages/Invest/InvestmentCard/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import { toLocaleStringOptions } from 'config';
+import { AccountContext } from 'contexts/AccountContext';
+import React, { useContext } from 'react';
 import { ProjectPageDetails, FullProjectPageDetails } from 'types/projectTypes';
 
 export const InvestmentCard = ({
@@ -6,6 +8,7 @@ export const InvestmentCard = ({
 }: {
   project: ProjectPageDetails | FullProjectPageDetails;
 }) => {
+  const { availableCashBalance } = useContext(AccountContext);
   return (
     <div className='card investment-card-wrapper'>
       {/* TODO: replace bg-primary with primary color circle CSS background from design */}
@@ -17,12 +20,19 @@ export const InvestmentCard = ({
             </div>
             <div className='col-lg-6 d-flex justify-content-end align-items-end'>
               <h4 className='text-white'>
-                CASH BALANCE: <span className='text-success'>123456</span>
+                CASH BALANCE:{' '}
+                <span className='text-success'>
+                  €
+                  {availableCashBalance.toLocaleString(
+                    undefined,
+                    toLocaleStringOptions
+                  )}
+                </span>
               </h4>
             </div>
           </div>
           <div className='row'>
-            <div className='col-lg-8 d-flex justify-content-start align-items-end'>
+            <div className='col-lg-9 d-flex justify-content-start align-items-end'>
               <div className='w-100'>
                 <label htmlFor='investment-input' className='text-white'>
                   Your Investment Amount
@@ -36,8 +46,8 @@ export const InvestmentCard = ({
                 />
               </div>
             </div>
-            <div className='col-lg-4 d-flex justify-content-end align-items-end'>
-              <button className='btn btn-primary text-primary w-100 btn-sm'>
+            <div className='col-lg-3 d-flex justify-content-center align-items-end'>
+              <button className='btn btn-primary text-primary btn-sm'>
                 Submit Your Order
               </button>
             </div>
