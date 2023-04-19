081e9a87443596a745d5bf0ec1ebfa38437b3ebb
alexbolog <mbolog.alex@gmail.com>
2023-04-19 12:14:55 +0300

    added mobile layout for project details page

--- src/pages/ProjectDetails/Layout/index.tsx
+++ src/pages/ProjectDetails/Layout/index.tsx
@@ -13,25 +13,21 @@ export const ProjectDetailsLayout = ({
   project: ProjectPageDetails | FullProjectPageDetails;
 }) => {
   return (
-    <div className='container-fluid w-100 p-0'>
-      <div className='row'>
-        <div className='col'>
-          <h3 className='breadcrumbs'>
-            <Link to={routeNames.home} className='main'>
-              All Projects
-            </Link>
-            <FontAwesomeIcon icon={faChevronRight} className='sepz' />
-            {project.projectTitle}
-          </h3>
-        </div>
+    <div className='row'>
+      <div className='col-lg-12'>
+        <h3 className='breadcrumbs'>
+          <Link to={routeNames.home} className='main'>
+            All Projects
+          </Link>
+          <FontAwesomeIcon icon={faChevronRight} className='sepz' />
+          {project.projectTitle}
+        </h3>
       </div>
-      <div className='row'>
-        <div className='col-8'>
-          <MainContainer project={project} />
-        </div>
-        <div className='col-4 p-0'>
-          <ProjectSpecs project={project} sticky />
-        </div>
+      <div className='col-lg-8 col-md-12 col-sm-12'>
+        <MainContainer project={project} />
+      </div>
+      <div className='col-lg-4 p-0' id='project-specs-floating-panel'>
+        <ProjectSpecs project={project} sticky />
       </div>
     </div>
   );
--- src/pages/ProjectDetails/MainContainer/index.tsx
+++ src/pages/ProjectDetails/MainContainer/index.tsx
@@ -23,6 +23,7 @@ import { RefractoRating } from './RefractoRating';
 import { CapitalStructure } from './CapitalStructure';
 import { RepaymentSchedule } from './RepaymentSchedule';
 import { Finances } from './Finances';
+import { ProjectSpecs } from 'components/ProjectSpecs';
 
 export const MainContainer = ({
   project
@@ -34,28 +35,28 @@ export const MainContainer = ({
   const connectedComponents = () => {
     return (
       <>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <ProjectDetails project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <Location project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <Sponsor project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <SWOT project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <RefractoRating project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <CapitalStructure project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <RepaymentSchedule project={project as FullProjectPageDetails} />
         </div>
-        <div className='col-12 p-0'>
+        <div className='col-12'>
           <Finances project={project as FullProjectPageDetails} />
         </div>
       </>
@@ -63,16 +64,19 @@ export const MainContainer = ({
   };
   return (
     <>
-      <div className='container-fluid p-0'>
-        <div className='row w-100'>
-          <div className='col-12 p-0'>
+      <div className='container-fluid p-0 w-100'>
+        <div className='row'>
+          <div className='col-12'>
             <HeaderCard project={project} />;
           </div>
-          <div className='col-12 p-0'>
+          <div className='col-12' id='project-specs-floating-panel-mobile'>
+            <ProjectSpecs project={project} />
+          </div>
+          <div className='col-12'>
             <ExecutiveSummary project={project} />;
           </div>
           {!isConnected ? (
-            <div className='col-12 p-0'>
+            <div className='col-12'>
               <NotConnected />
             </div>
           ) : (
