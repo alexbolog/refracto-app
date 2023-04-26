3f08974fb0bd553e9c18e229611f92ff08cbb3b2
alexbolog <mbolog.alex@gmail.com>
2023-04-26 11:59:18 +0300

    merge leftovers

--- src/pages/ProjectDetails/MainContainer/RepaymentSchedule/index.tsx
+++ src/pages/ProjectDetails/MainContainer/RepaymentSchedule/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { FullProjectPageDetails } from 'types/projectTypes';
-<<<<<<< HEAD
 import { RepaymentScheduleTable } from './RepaymentScheduleTable';
 import { MobileCollapsibleSection } from '../MobileCollapsibleSection';
-=======
-import { Schedule } from './Schedule';
-import './style.css';
->>>>>>> e1fb28d (added project types)
 
 export const RepaymentSchedule = ({
   project
@@ -14,20 +9,9 @@ export const RepaymentSchedule = ({
   project: FullProjectPageDetails;
 }) => {
   return (
-<<<<<<< HEAD
     <MobileCollapsibleSection
       header='Repayment Schedule'
       body={<RepaymentScheduleTable items={project.repaymentSchedule} />}
     />
-=======
-    <div className='card executive-summary-wrapper'>
-      <div className='card-header'>
-        <h1>Repayment Schedule</h1>
-      </div>
-      <div className='card-body'>
-        <Schedule items={project.repaymentSchedule} />
-      </div>
-    </div>
->>>>>>> e1fb28d (added project types)
   );
 };
