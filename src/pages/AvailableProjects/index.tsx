8be4cb726e4400ef12b47f522b9f846fa8e0fc74
alexbolog <mbolog.alex@gmail.com>
2023-04-26 17:20:10 +0300

    added empty page

--- src/pages/AvailableProjects/index.tsx
+++ src/pages/AvailableProjects/index.tsx
@@ -5,11 +5,11 @@ import { Filters } from './Filters';
 import { ProjectListFilterType } from './Filters/ProjectListFilterType';
 import { Project } from './Project';
 import { ProjectListItem } from 'types/projectTypes';
+import { ReactComponent as EmptyPageScreen } from './../../assets/icons/refracto/empty-page-available-projects.svg';
 
 const AvailableProjects = () => {
   const { availableProjects } = useContext(ProjectContext);
-  // const [currentAppliedFilters, setCurrentAppliedFilters] =
-  //   useState<ProjectListFilterType>();
+  // const { availableProjects } = { availableProjects: [] };
 
   const [filteredProjects, setFilteredProjects] =
     useState<ProjectListItem[]>(availableProjects);
@@ -57,9 +57,16 @@ const AvailableProjects = () => {
   return (
     <div className='container-fluid w-100 p-0'>
       <div className='row'>
-        <div className='col-12'>
-          <Filters onApplyFilters={handleApplyFilters} />
-        </div>
+        {availableProjects.length > 0 && (
+          <div className='col-12'>
+            <Filters onApplyFilters={handleApplyFilters} />
+          </div>
+        )}
+        {availableProjects.length === 0 && (
+          <div className='col-12'>
+            <h1>Available Projects</h1>
+          </div>
+        )}
 
         {filteredProjects.length > 0 &&
           filteredProjects.map((p, i) => (
@@ -67,6 +74,23 @@ const AvailableProjects = () => {
               <Project project={p} />
             </div>
           ))}
+        {(filteredProjects.length === 0 || availableProjects.length === 0) && (
+          <>
+            <div className='col-12 text-center mb-5 mt-5'>
+              {availableProjects.length > 0
+                ? 'Uh oh! Seems like no project is matching your filters'
+                : 'Ups! Please come back later. More projects coming soon.'}
+            </div>
+            <div className='col-12 text-center'>
+              <h1 className='text-primary' role='button'>
+                Meanwhile, Check Our Demo!
+              </h1>
+            </div>
+            <div className='col-12 text-center'>
+              <EmptyPageScreen />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
