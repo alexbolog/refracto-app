d23d87216fd820e32652073f5878fa6b421ded43
alexbolog <mbolog.alex@gmail.com>
2023-05-02 14:43:26 +0300

    added responsive image style & centering in gallery

--- src/pages/ProjectDetails/MainContainer/HeaderCard.tsx
+++ src/pages/ProjectDetails/MainContainer/HeaderCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FullProjectPageDetails, ProjectPageDetails } from 'types/projectTypes';
