5f775d4d92895efce6705c27e25512f43aec6ca8
alexbolog <mbolog.alex@gmail.com>
2023-05-03 18:15:18 +0300

    added Q&A design

--- src/pages/ProjectDetails/MainContainer/QandA/index.tsx
+++ src/pages/ProjectDetails/MainContainer/QandA/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FullProjectPageDetails } from 'types/projectTypes';
 import { MobileCollapsibleSection } from '../MobileCollapsibleSection';
+import './style.css';
 
 export const QandA = ({ project }: { project: FullProjectPageDetails }) => {
   const qandaFirst = project.questionsAndAnswers.filter((q) => q.id === '1')[0];
@@ -8,16 +9,74 @@ export const QandA = ({ project }: { project: FullProjectPageDetails }) => {
     <MobileCollapsibleSection
       header='Q&A'
       body={
-        <div className='container-fluid w-100 p-0'>
-          <div className='row'>
-            <div className='col-lg-2'>
-              <img src={qandaFirst.ownerImgSrc} className='rounded-circle' />
+        <>
+          <div className='main-review-box'>
+            <img src='user.jpg' className='rounded-circle' />
+            <div className='username'>Leona Pop</div>
+            <div className='timestamp'>4 days ago</div>
+            <div className='question'>
+              I would like to know the deadline of the project. Can anyone
+              provide this info? Thank you!
             </div>
-            <div className='col-lg-10'>
-              lol
+            <div className='question-link' role='button'>
+              Reply
             </div>
           </div>
-        </div>
+          <div className='main-review-box'>
+            <img src='user.jpg' className='rounded-circle' />
+            <div className='username'>Leona Pop</div>
+            <div className='timestamp'>4 days ago</div>
+            <div className='question'>
+              Also I’m interested about the Refracto Rating.
+            </div>
+            {/* <div className='question-link' role='button'>
+              Reply
+            </div> */}
+            <div className='child'>
+              <div className='main-review-box'>
+                <img src='user.jpg' className='rounded-circle' />
+                <div className='username'>Marius Enache</div>
+                <div className='timestamp'>4 days ago</div>
+                <div className='question'>
+                  I would like to know the deadline of the project. Can anyone
+                  provide this info? Thank you!
+                </div>
+                {/* <div className='question-link' role='button'>
+                  Reply
+                </div> */}
+              </div>
+              <div className='main-review-box'>
+                <img src='user.jpg' className='rounded-circle' />
+                <div className='username'>Marius Enache</div>
+                <div className='timestamp'>4 days ago</div>
+                <div className='question'>
+                  I would like to know the deadline of the project. Can anyone
+                  provide this info? Thank you!
+                </div>
+                <div className='question-link' role='button'>
+                  Reply
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='basic-form'>
+            <div>Replying to @Leona Pop</div>
+            <form>
+              <div className='mb-3'>
+                <textarea
+                  className='form-control'
+                  rows={6}
+                  id='comment'
+                ></textarea>
+              </div>
+            </form>
+          </div>
+          <div>
+            <div className='btn btn-primary w-50' style={{ marginLeft: '50%' }}>
+              Reply
+            </div>
+          </div>
+        </>
       }
     />
   );
