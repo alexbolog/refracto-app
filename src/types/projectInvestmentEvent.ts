8f2b93ddb2c9af65074f4f63d29ea334b050ebed
Vali M <marginean.vali@gmail.com>
2023-05-10 17:37:47 +0300

    Added field returnPercentage

--- src/types/projectInvestmentEvent.ts
+++ src/types/projectInvestmentEvent.ts
@@ -3,5 +3,6 @@ import { InvestmentEvent } from './investmentEvent';
 export default interface ProjectInvestmentEvent {
   projectId: string;
   projectTitle: string;
+  returnPercentage: number;
   investments: InvestmentEvent[];
 }
