c13a106b3c44fe10e9eaa6186c74ce3d5be9d071
alexbolog <mbolog.alex@gmail.com>
2023-05-10 13:12:57 +0300

    added listings layout

--- src/pages/Marketplace/AvailableListings.tsx
+++ src/pages/Marketplace/AvailableListings.tsx
@@ -7,99 +7,37 @@ export const AvailableListings = ({
   listings: ProjectListItem[];
 }) => {
   return (
-    // <>
-    //   <div className='row'>
-    //     <div className='col-5'>Investment Opportunity</div>
-    //     <div className='col-1'>Remaining Time</div>
-    //     <div className='col-1'>Remaining Principal</div>
-    //     <div className='col-1'>Price</div>
-    //     <div className='col-1'>Repayment</div>
-    //     <div className='col-1'>Expected ROR</div>
-    //   </div>
-    //   <div className='row'>
-    //     <div className='col-1'>
-    //       <img src={listings[0].thumbnailSrc} />
-    //     </div>
-    //     <div className='col-4'>{listings[0].projectTitle}</div>
-    //     <div className='col-4'>{listings[0].projectTitle}</div>
-    //   </div>
-    // </>
-    <div className='card w-100 listings'>
-      <div className='card-body'>
-        <div className='table-responsive'>
-          <table className='table table-responsive-sm'>
-            <thead>
-              <tr>
-                <th>Investment Opportunity</th>
-                <th>Remaining Time</th>
-                <th>Remaining Principal</th>
-                <th>Price</th>
-
-                <th>Repayment</th>
-                <th>Expected ROR</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className='investment-opportunity'>
-                  <img src={listings[0].thumbnailSrc} />
-                  <div>{listings[0].projectTitle}</div>
-                </td>
-                <td>10 days</td>
-                <td>$800.00</td>
-                <td>$830.00</td>
-                <td>$865.00</td>
-                <td>8.76%</td>
-                <td>
-                  <button className='btn btn-outline-primary w-100'>Buy</button>
-                </td>
-              </tr>
-              {/* {items.map((row, idx) => (
-                <tr key={`repayment-schedule-entry_${idx}`}>
-                  <td>{formatIso(row.date)}</td>
-                  <td>
-                    <span
-                      className={paymentStatusPillStyling(row.paymentStatus)}
-                    >
-                      {row.paymentStatus}
-                    </span>
-                  </td>
-                  <td>
-                    {row.principalAmount.toLocaleString(
-                      undefined,
-                      toLocaleStringOptions
-                    )}
-                  </td>
-                  <td>
-                    {row.interestAmount.toLocaleString(
-                      undefined,
-                      toLocaleStringOptions
-                    )}
-                  </td>
-                  <td>
-                    {row.lateFees.toLocaleString(
-                      undefined,
-                      toLocaleStringOptions
-                    )}
-                  </td>
-                  <td>
-                    {(row.principalAmount + row.interestAmount).toLocaleString(
-                      undefined,
-                      toLocaleStringOptions
-                    )}
-                  </td>
-                  <td>
-                    {(row.paymentStatus === 'Done'
-                      ? row.interestAmount + row.principalAmount + row.lateFees
-                      : 0
-                    ).toLocaleString(undefined, toLocaleStringOptions)}
-                  </td>
-                </tr>
-              ))} */}
-            </tbody>
-          </table>
+    <div className='card listings-wrapper'>
+      <div className='card-body container-fluid listings'>
+        <div className='row table-header d-flex align-items-center'>
+          <div className='col-lg-4'>Investment Opportunity</div>
+          <div className='col-lg-1 text-center'>Remaining Time</div>
+          <div className='col-lg-1 text-center'>Remaining Principal</div>
+          <div className='col-lg-1 text-center'>Price</div>
+          <div className='col-lg-2 text-center'>Repayment</div>
+          <div className='col-lg-1 text-center'>Expected ROR</div>
         </div>
+        {listings.map((listing, idx) => (
+          <div
+            className={`row table-content ${
+              idx === listings.length - 1 ? 'last' : ''
+            }`}
+            key={`secondary-marketplace-listing-${idx}-${listing.projectId}`}
+          >
+            <div className='col-1'>
+              <img src={listing.thumbnailSrc} />
+            </div>
+            <div className='col-3 proj-name'>{listing.projectTitle}</div>
+            <div className='col-lg-1 text-center'>10 days</div>
+            <div className='col-lg-1 text-center'>$800.00</div>
+            <div className='col-lg-1 text-center'>$830.00</div>
+            <div className='col-lg-2 text-center'>$865.00</div>
+            <div className='col-lg-1 text-center'>8.76%</div>
+            <div className='col-lg-2 text-center'>
+              <button className='btn btn-primary btn-sm w-100'>Buy</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
