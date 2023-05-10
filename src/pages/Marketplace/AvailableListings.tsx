743546cb337a18d2eec1591fb32d3a023a0097a7
alexbolog <mbolog.alex@gmail.com>
2023-05-10 16:20:54 +0300

    added mobile version

--- src/pages/Marketplace/AvailableListings.tsx
+++ src/pages/Marketplace/AvailableListings.tsx
@@ -9,7 +9,7 @@ export const AvailableListings = ({
   return (
     <div className='card listings-wrapper'>
       <div className='card-body container-fluid listings'>
-        <div className='row table-header d-flex align-items-center'>
+        <div className='row dsk table-header d-flex align-items-center'>
           <div className='col-lg-5'>Investment Opportunity</div>
           <div className='col-lg-1 text-center'>Remaining Time</div>
           <div className='col-lg-1 text-center'>Remaining Principal</div>
@@ -24,15 +24,17 @@ export const AvailableListings = ({
             }`}
             key={`secondary-marketplace-listing-${idx}-${listing.projectId}`}
           >
-            <div className='col-1'>
+            <div className='col-lg-1 col-md-12 col-sm-12'>
               <img src={listing.thumbnailSrc} />
             </div>
-            <div className='col-4 proj-name'>{listing.projectTitle}</div>
-            <div className='col-lg-1 text-center'>10 days</div>
-            <div className='col-lg-1 text-center'>$800.00</div>
-            <div className='col-lg-1 text-center'>$830.00</div>
-            <div className='col-lg-1 text-center'>$865.00</div>
-            <div className='col-lg-1 text-center'>8.76%</div>
+            <div className='col-lg-4 col-md-12 col-sm-12 proj-name'>{listing.projectTitle}</div>
+            <div className='col-lg-1 text-center'>
+              <div className='mbl table-header'>Remaining Time:</div> 10 days
+            </div>
+            <div className='col-lg-1 text-center'><div className='mbl table-header'>Remaining Principal:</div>$800.00</div>
+            <div className='col-lg-1 text-center'><div className='mbl table-header'>Price:</div>$830.00</div>
+            <div className='col-lg-1 text-center'><div className='mbl table-header'>Repayment:</div>$865.00</div>
+            <div className='col-lg-1 text-center'><div className='mbl table-header'>Expected ROR:</div>8.76%</div>
             <div className='col-lg-2 text-center'>
               <button className='btn btn-primary btn-sm w-100'>Buy</button>
             </div>
