f00346d94822951be591856165126d5fbef20b17
Vali M <marginean.vali@gmail.com>
2023-05-10 19:27:23 +0300

    Redid bar chart prototype using Apex Charts

--- src/pages/Overview/InvestmentAndReturnBarCharts/index.tsx
+++ src/pages/Overview/InvestmentAndReturnBarCharts/index.tsx
@@ -1,81 +1,77 @@
-import * as React from 'react';
+import React from 'react';
+import Chart from 'react-apexcharts';
+import { DateTime } from 'luxon';
 import ProjectInvestmentEvent from '../../../types/projectInvestmentEvent';
 import { useGetProjectInvestmentHistory } from '../../../contexts/InvestmentHistory/hooks/useGetProjectInvestmentHistory';
-import { useEffect, useState } from 'react';
-import { Bar } from 'react-chartjs-2';
-import {
-  BarController,
-  BarElement,
-  CategoryScale,
-  Chart,
-  ChartData,
-  LinearScale,
-  Tooltip
-} from 'chart.js';
+import { InvestmentEvent } from '../../../types/investmentEvent';
+import { ApexOptions } from 'apexcharts';
+
+interface SeriesData {
+  name: string;
+  data: number[];
+}
 
 const InvestmentAndReturnBarCharts = () => {
-  Chart.register(
-    BarController,
-    BarElement,
-    CategoryScale,
-    LinearScale,
-    Tooltip
-  );
   const projectInvestments: ProjectInvestmentEvent[] =
     useGetProjectInvestmentHistory();
-  const emptyChartData: ChartData<'bar'> = {
-    labels: [],
-    datasets: [
-      {
-        label: 'Invested',
-        data: [],
-        backgroundColor: '#6853E8'
-      },
-      {
-        label: 'Profit',
-        data: [],
-        backgroundColor: '#FFA600'
-      }
-    ]
-  };
+  // Prepare data for the chart
+  const series: SeriesData[] = [
+    { name: 'Invested', data: [] },
+    { name: 'Profit', data: [] }
+  ];
+  const categories: string[] = [];
 
-  const [chartData, setChartData] = useState(emptyChartData);
+  projectInvestments.forEach((project: ProjectInvestmentEvent) => {
+    let invested = 0;
+    let profit = 0;
 
-  useEffect(() => {
-    const newChartData = { ...emptyChartData };
-
-    debugger;
-    projectInvestments.forEach((project) => {
-      let invested = 0;
-      debugger;
-
-      project.investments.forEach((investment) => {
-        if (investment.eventType === 'INVEST') {
-          debugger;
-          if (investment.committedDifference) {
-            invested += investment.committedDifference;
-          }
+    project.investments.forEach((investment: InvestmentEvent) => {
+      if (investment.eventType === 'INVEST') {
+        if (investment.committedDifference) {
+          invested += investment.committedDifference;
         }
-      });
-
-      const profit = invested * project.returnPercentage;
-
-      newChartData.labels!.push(project.projectTitle);
-      (newChartData.datasets[0].data as number[]).push(invested);
-      (newChartData.datasets[1].data as number[]).push(profit);
+      }
     });
 
-    setChartData(newChartData);
-  }, []);
+    profit = invested * project.returnPercentage;
+
+    categories.push(project.projectTitle);
+    series[0].data.push(invested);
+    series[1].data.push(profit);
+  });
+
+  const options: ApexOptions = {
+    chart: {
+      type: 'bar',
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '55%'
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      categories: categories
+    },
+    yaxis: {
+      title: {
+        text: 'Amount'
+      }
+    },
+    title: {
+      text: 'Investment and Profit per Project',
+      align: 'left'
+    },
+    colors: ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
+  };
 
   return (
-    <div className='col-sm-12'>
-      <div className='card'>
-        <div className='card-header d-flex justify-content-between'>
-          <h3>Investments and Returns</h3>
-        </div>
-        <Bar data={chartData} options={{ responsive: true }} />
-      </div>
+    <div id='chart'>
+      <Chart options={options} series={series} type='bar' height={350} />
     </div>
   );
 };
