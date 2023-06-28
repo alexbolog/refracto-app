83d75b660298809f8de2c1da0b419d14d92ebaa7
alexbolog <mbolog.alex@gmail.com>
2023-06-28 14:37:09 +0300

    copy paste transactions page

---
+++ src/pages/Agreements/AgreementsTable.tsx
@@ -0,0 +1,98 @@
+import { toLocaleStringOptions } from 'config';
+import { DateTime } from 'luxon';
+import React from 'react';
+import {
+  InvestmentTransaction,
+  InvestmentTransactionStatus
+} from 'types/accountTypes';
+import { formatDate } from 'utils';
+
+export const AgreementsTable = ({
+  agreements
+}: {
+  agreements: InvestmentTransaction[];
+}) => {
+  const getStatusClass = (status: InvestmentTransactionStatus) => {
+    switch (status) {
+      case InvestmentTransactionStatus.Finished:
+        return 'status-finished';
+      case InvestmentTransactionStatus.Pending:
+        return 'status-pending';
+      default:
+        return 'status-cancelled';
+    }
+  };
+
+  return (
+    <div className='card'>
+      <div className='card-body'>
+        <div className='table-responsive'>
+          <div className='transactions-table'>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th scope='col' className='date'>
+                    Date
+                  </th>
+                  <th scope='col'>Status</th>
+                  <th scope='col'>Project Name</th>
+                  <th scope='col'>Amount</th>
+                  <th scope='col'>Currency</th>
+                  <th scope='col'>Type</th>
+                  <th scope='col'>Operation</th>
+                  <th scope='col'>Description</th>
+                  <th scope='col'>Transaction</th>
+                </tr>
+              </thead>
+              <tbody>
+                {agreements.map((tx, index) => (
+                  <tr
+                    key={`investment-transactions-${index}-${tx.transactionHash}`}
+                  >
+                    <th scope='row'>
+                      {formatDate(
+                        DateTime.fromISO(tx.date),
+                        DateTime.DATETIME_SHORT
+                      )}
+                    </th>
+                    <td>
+                      <div className={`status ${getStatusClass(tx.status)}`}>
+                        {InvestmentTransactionStatus[tx.status]}
+                      </div>
+                    </td>
+                    <td>{tx.projectName}</td>
+                    <td>
+                      <div className='td-center'>
+                        {tx.amount.toLocaleString(
+                          undefined,
+                          toLocaleStringOptions
+                        )}
+                      </div>
+                    </td>
+                    <td>
+                      <div className='td-center'>{tx.currency}</div>
+                    </td>
+                    <td>
+                      <div className='td-center status status-pending'>
+                        {tx.type}
+                      </div>
+                    </td>
+                    <td>
+                      <div className='td-center'>{tx.operation}</div>
+                    </td>
+                    <td>{tx.description}</td>
+                    <td>
+                      <button className='btn btn-primary w-100 btn-sm'>
+                        See transaction
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Agreements/index.tsx
+++ src/pages/Agreements/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { FiltersV2 } from 'components/FiltersV2';
+import React, { useContext } from 'react';
+import { AgreementsTable } from './AgreementsTable';
+import { AccountContext } from 'contexts/AccountContext';
+import './style.css';
 
-const Agreements = () => {
+export const Agreements = () => {
+  const { investmentTransactions } = useContext(AccountContext);
   return (
-    <div className='row'>
-      <div className='col-lg-12 col-sm-12 d-flex justify-content-center'>
-        <h2>Agreements page coming soon</h2>
+    <div className='container-fluid p-0'>
+      <div className='row'>
+        <div className='col'>
+          <h1>Agreements</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12'>
+          <FiltersV2
+            items={[]}
+            onFilterChange={function (newItems: any[]): void {
+              //
+            }}
+          />
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12 mt-4'>
+          <AgreementsTable agreements={investmentTransactions} />
+        </div>
       </div>
     </div>
   );
