79ef2bca22291ecdd88d27225842f6c8f40ce525
alexbolog <mbolog.alex@gmail.com>
2023-08-23 14:34:30 +0300

    improved styling

--- src/pages/UnlockPage/components/XPortalConnectModal/ExistingPairing.tsx
+++ src/pages/UnlockPage/components/XPortalConnectModal/ExistingPairing.tsx
@@ -1,5 +1,7 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const ExistingPairing = ({
   onExistingPairConnect,
@@ -8,24 +10,42 @@ const ExistingPairing = ({
   onExistingPairConnect: () => void;
   onRemoveExistingPair: () => void;
 }) => {
+  const [isConnecting, setIsConnecting] = useState(false);
+  const handleConnect = () => {
+    setIsConnecting(true);
+    onExistingPairConnect();
+  };
   return (
     <div className='card w-75 mb-0'>
       <div className='card-body existing-pairing'>
-        <div className='w-25 text-center' onClick={onExistingPairConnect}>
-          <img
-            src='https://cdn.multiversx.com/xportal/logo_wc.jpg'
-            alt='xPortal wallet connect logo'
-          />
-        </div>
-        <div className='w-50 text-center' onClick={onExistingPairConnect}>
-          <h5>xPortal Wallet</h5>
-          <h6>(https://xportal.com)</h6>
-        </div>
-        <div className='w-25 text-end'>
-          <button className='btn btn-white' onClick={() => onRemoveExistingPair()}>
-            <FontAwesomeIcon icon={faXmark} />
-          </button>
-        </div>
+        {!isConnecting && (
+          <>
+            <div className='w-25 text-center' onClick={handleConnect}>
+              <img
+                src='https://cdn.multiversx.com/xportal/logo_wc.jpg'
+                alt='xPortal wallet connect logo'
+              />
+            </div>
+            <div className='w-50 text-center' onClick={handleConnect}>
+              <h5>xPortal Wallet</h5>
+              <h6>(https://xportal.com)</h6>
+            </div>
+            <div className='w-25 text-end'>
+              <button
+                className='btn btn-white'
+                onClick={() => onRemoveExistingPair()}
+              >
+                <FontAwesomeIcon icon={faXmark} />
+              </button>
+            </div>
+          </>
+        )}
+        {isConnecting && (
+          <div className='confirmation-message'>
+            <Spinner animation='border' variant='primary' />
+            <h5>Confirm on xPortal</h5>
+          </div>
+        )}
       </div>
     </div>
   );
