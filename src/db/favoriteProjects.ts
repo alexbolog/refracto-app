0baa276f30e3b664c7f3e3245052709d2f4ced3a
alexbolog <mbolog.alex@gmail.com>
2023-09-20 15:30:04 +0300

    added favorite projects rpc calls

--- src/db/favoriteProjects.ts
+++ src/db/favoriteProjects.ts
@@ -2,46 +2,31 @@ import { supabase } from 'apiRequests/supabaseClient';
 // CRUD operations for FavoriteProjects
 
 // Create a new favorite project
-export const createFavoriteProject = async (
-  projectId: number,
-  wallet_address: string
-) => {
-  const { data, error } = await supabase
-    .from('FavoriteProjects')
-    .insert([{ projectId: projectId, wallet_address: wallet_address }]);
-  if (error) console.log('Error: ', error);
-  else return data;
+export const createFavoriteProject = async (projectId: number) => {
+  let { error } = await supabase.rpc('insert_favorite_project', {
+    p_projectid: projectId
+  });
+  if (error) {
+    console.error('Error saving favorite project to db: ', error);
+  }
 };
 
-// Read favorite projects for a specific wallet address
-export const readFavoriteProjects = async (wallet_address: string) => {
-  const { data, error } = await supabase
-    .from('FavoriteProjects')
-    .select('*')
-    .eq('wallet_address', wallet_address);
-  if (error) console.log('Error: ', error);
-  else return data;
-};
-
-// Update a favorite project
-export const updateFavoriteProject = async (
-  id: number,
-  newProjectId: number
-) => {
-  const { data, error } = await supabase
-    .from('FavoriteProjects')
-    .update({ projectId: newProjectId })
-    .eq('id', id);
-  if (error) console.log('Error: ', error);
-  else return data;
+// Read favorite projects
+export const readFavoriteProjects = async () => {
+  let { data, error } = await supabase.rpc('get_favorite_projects');
+  if (error) {
+    console.error('Error fetching favorite projects from db: ', error);
+    return [];
+  }
+  return data;
 };
 
 // Delete a favorite project
 export const deleteFavoriteProject = async (id: number) => {
-  const { data, error } = await supabase
-    .from('FavoriteProjects')
-    .delete()
-    .eq('id', id);
-  if (error) console.log('Error: ', error);
-  else return data;
+  const { error } = await supabase.rpc('delete_favorite_project', {
+    p_projectid: id
+  });
+  if (error) {
+    console.error('Error saving favorite project to db: ', error);
+  }
 };
