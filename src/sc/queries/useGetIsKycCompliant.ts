b912fff488209cdad1c147d8b00ef41babd02ac1
alexbolog <mbolog.alex@gmail.com>
2023-10-16 15:57:28 +0300

    added getIsKycCompliant

---
+++ src/sc/queries/useGetIsKycCompliant.ts
@@ -0,0 +1,33 @@
+import { ResultsParser } from '@multiversx/sdk-core/out';
+import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks';
+import React from 'react';
+import { useGetProxyProvider } from 'sc/helpers/hooks/useGetProxyProvider';
+import { useGetSmartContract } from 'sc/helpers/hooks/useGetSmartContract';
+
+export const useGetIsKycCompliant = () => {
+  const { address } = useGetAccountInfo();
+  const smartContract = useGetSmartContract();
+  const proxy = useGetProxyProvider();
+
+  const isKycCompliant = async () => {
+    if (!address || !address.startsWith('erd1')) {
+      return false;
+    }
+    const interaction = smartContract.methods.isKycCompliant([address]).check();
+    const query = await interaction.buildQuery();
+    const queryResponse = await proxy.queryContract(query);
+    const { firstValue, returnCode, returnMessage } =
+      new ResultsParser().parseQueryResponse(
+        queryResponse,
+        interaction.getEndpoint()
+      );
+
+    if (!returnCode.isSuccess()) {
+      console.error('Failed to get isKycCompliant');
+      return false;
+    }
+    return firstValue?.valueOf();
+  };
+
+  return isKycCompliant;
+};
