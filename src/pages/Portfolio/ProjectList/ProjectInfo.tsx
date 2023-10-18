f8ec3b52d3321a7a81fc81b1506085edfd6140cf
Vali <marginean.vali@gmail.com>
2023-10-18 22:53:19 +0300

    Made Project list in portfolio responsive

--- src/pages/Portfolio/ProjectList/ProjectInfo.tsx
+++ src/pages/Portfolio/ProjectList/ProjectInfo.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { toLocaleStringOptions } from 'config';
 import { DateTime } from 'luxon';
+import { RiskBox } from 'components/RiskBox';
+import { toLocaleStringOptions } from 'config';
 import { ActiveProjectInvestment } from 'types/projectTypes';
 import { formatIso } from 'utils';
-import { RiskBox } from 'components/RiskBox';
 
 const ProjectInfo = ({
   projectData,
@@ -19,7 +19,7 @@ const ProjectInfo = ({
       </div>
       <div className='col-lg-11 mb-3'>
         <div className='container-fluid w-100'>
-          <div className='row'>
+          <div className='row mb-2'>
             <div className='col-12 d-flex align-items-center'>
               <div
                 className='square'
@@ -31,36 +31,40 @@ const ProjectInfo = ({
                 <strong>{projectData.projectTitle}</strong>
               </span>
             </div>
-            <div className='col-12 d-flex justify-content-between align-items-center'>
-              <span>
-                Invested{' '}
-                <strong>
-                  €
-                  {projectData.amountInvested.toLocaleString(
-                    undefined,
-                    toLocaleStringOptions
-                  )}
-                </strong>
-              </span>
-              <span>
-                Return of investment{' '}
-                <strong>
-                  €
-                  {(
-                    projectData.returnPercentage * projectData.amountInvested
-                  ).toLocaleString(undefined, toLocaleStringOptions)}
-                </strong>
-              </span>
-              <span>
-                Deadline{' '}
-                <strong>
-                  {formatIso(
-                    projectData.crowdfundingDeadline,
-                    DateTime.DATE_SHORT
-                  )}
-                </strong>
-              </span>
+          </div>
+          <div className='row d-flex justify-content-between align-items-center flex-column flex-sm-row'>
+            <div className='col mb-1'>
+              Invested{' '}
+              <strong>
+                €
+                {projectData.amountInvested.toLocaleString(
+                  undefined,
+                  toLocaleStringOptions
+                )}
+              </strong>
+            </div>
+            <div className='col mb-1'>
+              Return of investment{' '}
+              <strong>
+                €
+                {(
+                  projectData.returnPercentage * projectData.amountInvested
+                ).toLocaleString(undefined, toLocaleStringOptions)}
+              </strong>
+            </div>
+            <div className='col mb-1'>
+              Deadline{' '}
+              <strong>
+                {formatIso(
+                  projectData.crowdfundingDeadline,
+                  DateTime.DATE_SHORT
+                )}
+              </strong>
+            </div>
+            <div className='col mb-1'>
               <RiskBox riskLevel={projectData.riskRatingLevel} />
+            </div>
+            <div className='col mb-1'>
               <button className='btn btn-primary view-proj-btn mb-1'>
                 View project details
               </button>
