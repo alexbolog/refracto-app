3f802fdb3f11fc23e5e1430c8b2c47eec03cba29
alexbolog <mbolog.alex@gmail.com>
2023-12-06 11:05:35 +0200

    added useSupabaseRealtime use

--- src/contexts/ProjectContext/hooks/useGetAvailableProjects.tsx
+++ src/contexts/ProjectContext/hooks/useGetAvailableProjects.tsx
@@ -1,4 +1,5 @@
 import { getAvailableProjects } from 'apiRequests/backend';
+import { useSupabaseRealtime } from 'hooks/supabase/useSupabaseRealtime';
 import React, { useEffect } from 'react';
 import { ProjectListItem } from 'types/projectTypes';
 
@@ -6,12 +7,16 @@ const useGetAvailableProjects = () => {
   const [activeProjectInvestments, setActiveProjectInvestments] =
     React.useState<ProjectListItem[]>([]);
 
+  useSupabaseRealtime({
+    channel: 'Projects',
+    onAll: () => {
+      console.log('REALTIME TRIGGERED UPDATE PROJECTS');
+      updateProjects();
+    }
+  });
+
   useEffect(() => {
     updateProjects();
-    const interval = setInterval(() => {
-      updateProjects();
-    }, 3000);
-    return () => clearInterval(interval);
   }, []);
 
   const updateProjects = () => {
